dafe813860b483740f4d121821ea4261745fb56c
Zeb Engberg <zebengberg@gmail.com>
2020-04-26 11:26:38 -0600

    put data in pandas df

--- mountain-slicer/mountain_slicer.py
+++ mountain-slicer/mountain_slicer.py
@@ -5,7 +5,8 @@ with elevations above 1000m.
 """
 
 import ee
-import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 ee.Initialize()
 
@@ -18,22 +19,25 @@ dem = dem.clip(rect)
 dem = dem.convolve(ee.Kernel.gaussian(5, 4))
 
 # An ee.List containing contour elevations to calculate.
-lines = ee.List.sequence(2000, 3100, 20)
+lines = ee.List.sequence(2000, 3100, 10)
 
 # An ee.List of masked ee.Image.
 contours = lines.map(lambda l: dem.gt(ee.Number(l)).selfMask())
 
 
-# Converting contours to a vector object. After calling ee.reduceToVectors(),
-# each contour is either a Polygon or MultiPolygon object. For MultiPolygon
-# objects, we only keep the polygon containing the origin.
+
 def convert_to_polygon(c):
+    """Convert contour image to polygon containing summit."""
     c = ee.Image(c).toInt()
     features = c.reduceToVectors(scale=10)
     geos = features.geometry().geometries()
     contains_summit = geos.map(lambda p: ee.Geometry(p).contains(summit))
     return geos.get(contains_summit.indexOf(True))
 
+
+# Converting contours to a vector object. After calling ee.reduceToVectors(),
+# each contour is either a Polygon or MultiPolygon object. For MultiPolygon
+# objects, we only keep the polygon containing the origin.
 contour_polygons = contours.map(convert_to_polygon)
 
 
@@ -41,5 +45,16 @@ contour_polygons = contours.map(convert_to_polygon)
 perimeters = contour_polygons.map(lambda p: ee.Geometry(p).perimeter(1))
 areas = contour_polygons.map(lambda p: ee.Geometry(p).area(1))
 
-print(areas.getInfo())
-print(perimeters.getInfo())
+data = {'elevation': lines.getInfo(),
+        'area': areas.getInfo(),
+        'perimeter': perimeters.getInfo()}
+
+
+data = pd.DataFrame.from_dict(data)
+
+data.plot(x='elevation', y='perimeter', kind='scatter')
+plt.show()
+
+# TODO: Perhaps use ee.Export rather than getInfo()
+# getInfo() runs into server side issues with too many calls
+# Export may avoid these.
