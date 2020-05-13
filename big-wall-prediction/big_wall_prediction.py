4928348f17faac9caf2e2bad5f5b4861009818ee
Zeb Engberg <zebengberg@gmail.com>
2020-05-13 08:04:01 -0600

    started implementing xgboost

--- big-wall-prediction/big_wall_prediction.py
+++ big-wall-prediction/big_wall_prediction.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import xgboost as xgb
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from imblearn.over_sampling import SMOTE, SVMSMOTE, ADASYN, RandomOverSampler
 
@@ -8,15 +9,15 @@ explored = pd.read_csv('data/explored.csv')
 unexplored = pd.read_csv('data/unexplored.csv')
 
 
-def build_x_y():
+def build_x_y(prob=0.9):
   """Create balanced classes by oversampling."""
   X_pred = unexplored.copy()  # to be used to make predictions
 
   X = explored.drop(columns=['latitude', 'longitude', 'mp_score'])
-  mask = np.random.rand(len(X)) < 0.9
+  mask = np.random.rand(len(X)) < prob
   X_train = X[mask]
   X_test = X[~mask]
-  
+
   # For now, running classification models instead of regression.
   y = explored.mp_score > 0
   y = y.map(lambda x: 1 if x else 0)
@@ -24,8 +25,8 @@ def build_x_y():
   y_test = y[~mask]
 
   # Oversampling the imbalanced training data.
-  X_train, y_train = SVMSMOTE().fit_resample(X_train, y_train)
-  
+  X_train, y_train = RandomOverSampler().fit_resample(X_train, y_train)
+
   return X_train, y_train, X_test, y_test, X_pred
 
 
@@ -34,13 +35,12 @@ def run_lin_reg():
   X_train, y_train, X_test, y_test, X_pred = build_x_y()
   reg = LinearRegression().fit(X_train, y_train)
   print('Linear regression fit score: ' + str(reg.score(X_test, y_test)) + '\n')
-  
   y_pred = reg.predict(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
   X_pred['predicted_score'] = y_pred
   X_pred.height *= 1000
   X_pred = X_pred.sort_values(by='predicted_score', ascending=False)[['latitude', 'longitude', 'height', 'mp_score', 'predicted_score']]
   X_pred.to_csv('data/predictions/linear_reg_predictions.csv', header=True, index=False)
-  
+
   print('Top 20 results from linear regression prediction.')
   print('A higher score is better!\n')
   print(X_pred[:20].to_string(index=False))
@@ -56,6 +56,7 @@ def run_log_reg():
   # On the otherhand, predict() uses a cutoff at 0.5 to make a choice.
   y_pred = reg.predict_proba(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
   y_pred = y_pred[:, 1:]
+  
   X_pred['predicted_score'] = y_pred
   X_pred.height *= 1000
   X_pred = X_pred.sort_values(by='predicted_score', ascending=False)[['latitude', 'longitude', 'height', 'mp_score', 'predicted_score']]
@@ -65,10 +66,27 @@ def run_log_reg():
   print('A higher score is better!\n')
   print(X_pred[:20].to_string(index=False))
 
+def run_xgb():
+  X_train, y_train, X_test, y_test, X_pred = build_x_y()
+  # dmatrix = xgb.DMatrix(data=X_train, label=y_train)
+  # can also use XGBClassifier
+  reg = xgb.XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.1,
+              max_depth = 5, alpha = 10, n_estimators = 10)
+  reg.fit(X_train, y_train)
+  
+  y_pred = reg.predict(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
+  X_pred['predicted_score'] = y_pred
+  X_pred.height *= 1000
+  X_pred = X_pred.sort_values(by='predicted_score', ascending=False)[['latitude', 'longitude', 'height', 'mp_score', 'predicted_score']]
+  X_pred.to_csv('data/predictions/xgb_predictions.csv', header=True, index=False)
+
+  print('Top 20 results from xgb prediction.')
+  print('A higher score is better!\n')
+  print(X_pred[:20].to_string(index=False))
 
 
-# Logistic regression
-# xgboost gradient boost for regression
+
+
 # nn regression
 # random forest
 
@@ -76,3 +94,4 @@ def run_log_reg():
 if __name__ == '__main__':
   run_lin_reg()
   run_log_reg()
+  run_xgb()
