c5020b253f4323e7b2f7c24c9b430efbf5ee1161
Zeb Engberg <zebengberg@gmail.com>
2020-05-13 06:42:44 -0600

    implemented basic logistic regression

--- big-wall-prediction/big_wall_prediction.py
+++ big-wall-prediction/big_wall_prediction.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from imblearn.over_sampling import SMOTE, SVMSMOTE, ADASYN, RandomOverSampler
 
@@ -8,33 +10,60 @@ unexplored = pd.read_csv('data/unexplored.csv')
 
 def build_x_y():
   """Create balanced classes by oversampling."""
-  X = explored.drop(columns=['latitude', 'longitude', 'mp_score'])
-  X_pred = unexplored.copy()
+  X_pred = unexplored.copy()  # to be used to make predictions
 
+  X = explored.drop(columns=['latitude', 'longitude', 'mp_score'])
+  mask = np.random.rand(len(X)) < 0.9
+  X_train = X[mask]
+  X_test = X[~mask]
+  
   # For now, running classification models instead of regression.
   y = explored.mp_score > 0
   y = y.map(lambda x: 1 if x else 0)
+  y_train = y[mask]
+  y_test = y[~mask]
 
-  # Oversampling the imbalanced class.
-  X, y = SVMSMOTE().fit_resample(X, y)
+  # Oversampling the imbalanced training data.
+  X_train, y_train = SVMSMOTE().fit_resample(X_train, y_train)
   
-  return X, y, X_pred
+  return X_train, y_train, X_test, y_test, X_pred
 
 
 def run_lin_reg():
   """Run a simple linear regression."""
-  X, y, X_pred = build_x_y()
-  reg = LinearRegression().fit(X, y)
-  print('Linear regression fit score: ' + str(reg.score(X, y)) + '\n')
+  X_train, y_train, X_test, y_test, X_pred = build_x_y()
+  reg = LinearRegression().fit(X_train, y_train)
+  print('Linear regression fit score: ' + str(reg.score(X_test, y_test)) + '\n')
+  
   y_pred = reg.predict(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
   X_pred['predicted_score'] = y_pred
   X_pred.height *= 1000
   X_pred = X_pred.sort_values(by='predicted_score', ascending=False)[['latitude', 'longitude', 'height', 'mp_score', 'predicted_score']]
   X_pred.to_csv('data/predictions/linear_reg_predictions.csv', header=True, index=False)
   
-  print('Top 30 results from linear regression prediction.')
+  print('Top 20 results from linear regression prediction.')
   print('A higher score is better!\n')
-  print(X_pred[:30].to_string(index=False))
+  print(X_pred[:20].to_string(index=False))
+
+
+def run_log_reg():
+  """Run a simple logistic classification."""
+  X_train, y_train, X_test, y_test, X_pred = build_x_y()
+  reg = LogisticRegression(max_iter=200).fit(X_train, y_train)
+  print('Logistic regression fit score: ' + str(reg.score(X_test, y_test)) + '\n')
+  
+  # Use predict_proba() to get probabilities of hittng a class, not classes themselves.
+  # On the otherhand, predict() uses a cutoff at 0.5 to make a choice.
+  y_pred = reg.predict_proba(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
+  y_pred = y_pred[:, 1:]
+  X_pred['predicted_score'] = y_pred
+  X_pred.height *= 1000
+  X_pred = X_pred.sort_values(by='predicted_score', ascending=False)[['latitude', 'longitude', 'height', 'mp_score', 'predicted_score']]
+  X_pred.to_csv('data/predictions/logistic_reg_predictions.csv', header=True, index=False)
+  
+  print('Top 20 results from logistic regression prediction.')
+  print('A higher score is better!\n')
+  print(X_pred[:20].to_string(index=False))
 
 
 
@@ -42,3 +71,8 @@ def run_lin_reg():
 # xgboost gradient boost for regression
 # nn regression
 # random forest
+
+
+if __name__ == '__main__':
+  run_lin_reg()
+  run_log_reg()
